b908a85d650c6b52906355cdf06d7e0f256c415e
Humberto Guedes <franciscohguedes@gmail.com>
2023-01-14 19:06:02 -0300

    add: interfaces, controlador e rotas

---
+++ Controllers/CardController.cs
@@ -0,0 +1,37 @@
+﻿using CDC.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CDC.Controllers
+{
+    public class CardController : Controller
+    {
+        private readonly CDCContext _context;
+
+        //injeção de dependência
+        public CardController(CDCContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Cards.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
